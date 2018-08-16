56244286af00a40f3922e4dc5858a23a5dbbdcf9
wangliangxun <wangliangxun@whu.edu.cn>
2018-08-16 23:37:38 +0800

    modify m.cpp d.h d.cpp to run/findpllate

--- platedetect_me_cpp/platedetect_me_cpp/DetectRegions.cpp
+++ platedetect_me_cpp/platedetect_me_cpp/DetectRegions.cpp
@@ -1,107 +1,18 @@
-#include <opencv2/opencv.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/ml/ml.hpp>
-#include <time.h>
-#include <iostream>
-#include <vector>
 #include "DetectRegions.h"
-//#include <DetectRegions.h>
-using namespace cv;
-using namespace std;
-bool showSteps=true;
-CvSVM svm;
-//////////////////////////////////getFilename////////////////////////////////////////
-
-string getFilename(string s)
+int HEIGHT=36;
+int WIDTH=136;
+void DetectRegions::setFilename(string s)
 {
-	char sep='/';
-	char sepExt='.';
-#if _WIN32
-	sep='\\';
-#endif
-	size_t i=s.rfind(sep,s.length());
-	if (i!=string::npos)
-	{
-		string fn=(s.substr(i+1,s.length()-i));
-		size_t j=fn.rfind(sepExt,fn.length());
-		if (i!=string::npos)
-		{return fn.substr(0,j);
-		} 
-		else
-		{return fn;
-		}
-	}
-	else
-	{
-		return "";
-	}
-
+	filename=s;
 }
-
-////////////////////////////////setmidle//////////////////////////////////////////
-Mat setmidle(Mat pic)
+Mat DetectRegions::run(Mat input)
 {
-
-}
-
-///////////////////////////////histeq///////////////////////////////////////////
-Mat histeq(Mat in)
-{
-
-}
-
-///////////////////////////////classify///////////////////////////////////////////
-int classify(Mat& image)
-{
-
-}
-
-////////////////////////////////jingquedingwei//////////////////////////////////////////
-Mat jingquedingwei(Mat in)
-{
-
-}
-
-////////////////////////////////main//////////////////////////////////////////
-int main()
-{
-	system("color 0E");
-	cout<<"			车牌识别 by wlx			";
-	char *filename;
-	Mat input_image;
-
-	//读取待识别图片
-	filename="./chepai/3.jpg";
-	input_image=imread(filename,1);
-	if (!input_image.data)
-	{cout<<"error read!"<<endl;
-	}
-	imshow("原图",input_image);
-	double duration=static_cast<double>(getTickCount());
-#if 1
-	/************************************************************************/
-	/* 首先通过颜色查找可能的车牌区域，并利用SVM判断这些可能的区域是否为车牌*/
-	/************************************************************************/
-	string filename_whithoutExt=getFilename(filename);
+	Mat tmp=findplate(input);
+	return tmp;
 	/************************************************************************/
 	/*                                                                      */
 	/************************************************************************/
-
-
-
-
-#endif
-
 }
-
-
-
-
-
-
-
 
 
 
---
+++ platedetect_me_cpp/platedetect_me_cpp/DetectRegions.h
@@ -0,0 +1,51 @@
+#ifndef DetectRegions_h
+#define DetectRegions_h
+#include <string.h>
+#include <vector>
+#include <cv.h>
+#include <highgui.h>
+#include <cvaux.h>
+using namespace cv;
+using namespace std;
+
+class DetectRegions
+{
+public:
+	enum Color{BLUE,YELLOW};
+	DetectRegions();
+	string filename;
+	void setFilename(string f);
+	bool saveRegions;
+	bool showSteps;
+	Mat run(Mat input);
+private:
+	Mat findplate(Mat input);
+	bool verifySizes(RotatedRect mr);
+	bool calcSafeRect(const RotatedRect& roi_rect,const Mat& src,Rect_<float>& safeBoundRect);
+	bool rotation(Mat& in,Mat& out,const double slope);
+	bool isdeflection(Mat& in,Mat& out,const Size rect_size,const Point2f center,const double angle);
+	Mat sobelyuchuli(Mat in);
+	Mat colorMatch(const Mat& src,const Color r);
+	Mat histeq(Mat in);
+};
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
