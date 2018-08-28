bbd944f56c98961be0d5a1da34eb56396bd03f06
wangliangxun <wangliangxun@whu.edu.cn>
2018-08-28 23:59:23 +0800

    update and add isdeflection

--- platedetect_me_cpp/platedetect_me_cpp/DetectRegions.cpp
+++ platedetect_me_cpp/platedetect_me_cpp/DetectRegions.cpp
@@ -348,8 +348,69 @@ bool DetectRegions::rotation(Mat& in, Mat& out, const Size rect_size, const Poin
 
 	return true;
 
+}
+/************************************************************************/
+/* 判断是否偏斜
+	输入二值化图像，输出判断结果
+*/
+/************************************************************************/
+bool DetectRegions::isdeflection(const Mat& in, const double angle, double& slope)
+{
+	int nRows=in.rows;
+	int nCols=in.cols;
+	assert(in.channels()==1);
 
+	int comp_index[3];
+	int len[3];
+
+	comp_index[0]=nRows/4*1;
+	comp_index[1]=nRows/4*2;
+	comp_index[2]=nRows/4*3;
+
+	const uchar*p;
+
+	for (int i=0;i<3;i++)
+	{
+		int index=comp_index[i];
+		p=in.ptr<uchar>(index);
+
+		int j=0;
+		int value=0;
+		while (0==value&&j<nCols)
+		{value=int(p[j++]);
+		}
+		len[i]=j;
+	}
+	double maxlen=max(len[2],len[0]);
+	double minlen=min(len[2],len[0]);
+	double difflen=abs(len[2]-len[0]);
+
+	double PI=3.14159265;
+	double g=tan(angle*PI/180.0);
+
+	if (maxlen-len[1]>nCols/32||len[1]-minlen>nCols/32)
+	{
+		double slope_can_1=double(len[2]-len[0])/double(comp_index[1]);
+		double slope_can_2=double(len[1]-len[0])/double(comp_index[0]);
+		double slope_can_3=double(len[2]-len[1])/double(comp_index[0]);
+		slope=abs(slope_can_1-g)<=abs(slope_can_2-g)?slope_can_1:slope_can_2;
+		return true;
+	} 
+	else
+	{
+		slope=0;
+	}
+	return false;
 
 }
+//扭变操作//通过opencv的仿射变换
+void DetectRegions::affine(Mat& in,Mat& out,const double slope)
+{
+	;
+}
+
+
+
+
 
 
