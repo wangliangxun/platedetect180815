2ce965155e3c3b502266f151740b0fdc3bd18895
wangliangxun <wangliangxun@whu.edu.cn>
2018-09-02 18:09:13 +0800

    complete the jingquedingwei function(消除边框影响)，update the file.doc

--- platedetect_me_cpp/platedetect_me_cpp/main.cpp
+++ platedetect_me_cpp/platedetect_me_cpp/main.cpp
@@ -59,8 +59,77 @@ int classify(Mat& image)
 }
 
 ////////////////////////////////jingquedingwei//////////////////////////////////////////
+/************************************************************************/
+/*                                                                      */
+/************************************************************************/
 Mat jingquedingwei(Mat in)
 {
+	Mat pic;
+	if(in.channels()==3)
+		cvtColor(in,pic,CV_BGR2GRAY);
+	else
+		pic=in.clone();
+
+	Mat plate_threshold=pic.clone();
+	threshold(pic,plate_threshold,10,255,CV_THRESH_BINARY);
+
+	int rmin=(int)(0.1*pic.cols);
+	vector<int>sum(plate_threshold.rows,0);
+	Mat histx(plate_threshold.cols,plate_threshold.rows,CV_8U,Scalar(0));
+
+	for (int i=0;i<plate_threshold.rows;i++)
+	{
+		for (int j=0;j<plate_threshold.cols;j++)
+		{
+			if(plate_threshold.at<uchar>(i,j)==255)
+				sum[i]++;
+		}
+	}
+	int shangbianjie=0;
+	int xiabianjie=pic.rows;
+
+	for (int i=sum.size()/2+1;i>0;i--)
+	{
+		if (sum[i]<=rmin)
+		{
+			shangbianjie=i;
+			break;
+		}
+	}
+	for (int i=sum.size()/2+1;i<sum.size();i++)
+	{
+		if (sum[i]<=rmin)
+		{
+			xiabianjie=i;
+			break;
+		}
+	}
+
+	Mat plateroi;
+	plateroi=pic(Rect(0,shangbianjie,pic.cols,xiabianjie-shangbianjie));
+
+	//Ïû³ý×ó±ß±ß¿òµÄÓ°Ïì
+	int nr=plateroi.rows;
+	int nc=plateroi.cols*plateroi.channels();
+	for (int j=0;j<nr;j++)
+	{
+		uchar* data=plateroi.ptr<uchar>(j);
+		for (int i=0;i<5;i++)
+		{
+			data[i]=0;
+		}
+	}
+	Mat resizepic(Size(plateroi.cols+20,plateroi.rows+20),CV_8UC1,cv::Scalar(0));
+	for (j=0;j<nr;j++)
+	{
+		uchar* data1=plateroi.ptr<uchar>(j);
+		uchar* data2=resizepic.ptr<uchar>(j+10);
+		for (int i=0;i<nc;i++)
+		{
+			data2[i+10]=data1[i];
+		}
+	}
+	return resizepic;
 
 }
 
@@ -91,13 +160,32 @@ int main()
 	detectRegions.saveRegions=true;
 	detectRegions.showSteps=true;
 	Mat plate=detectRegions.run(input_image);
-	/************************************************************************/
-	/*                                                                      */
-	/************************************************************************/
+	if (!plate.data)
+	{
+		cout<<"cannot find plate!"<<endl;
+		return 0;
+	}
 
+#endif
+	Mat plateroi=jingquedingwei(plate);
+	imwrite("myplate.jpg",plateroi);
+	if(showSteps)
+		imshow("",plateroi);
+#if 1
 
 
 #endif
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
