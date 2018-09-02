57b4cb5d1310c714791eab4b2b06ccd3bcd757e1
wangliangxun <wangliangxun@whu.edu.cn>
2018-09-02 19:16:46 +0800

    增加到字符分割部分函数

--- platedetect_me_cpp/platedetect_me_cpp/main.cpp
+++ platedetect_me_cpp/platedetect_me_cpp/main.cpp
@@ -167,12 +167,36 @@ int main()
 	}
 
 #endif
+	/************************************************************************/
+	/* 找到车牌的呆滞区域后，精确定位                                       */
+	/************************************************************************/
 	Mat plateroi=jingquedingwei(plate);
 	imwrite("myplate.jpg",plateroi);
 	if(showSteps)
 		imshow("",plateroi);
 #if 1
-
+	/************************************************************************/
+	/* 精确定位后，对车牌进行一些必要的处理然后开始分割字符                 */
+	/************************************************************************/
+	Mat zifu=plateroi.clone();
+	//因为第一个汉子有两个轮廓，例如‘粤’检测时会把上下两部分分割开，提前先膨胀然后再检测轮廓
+	Mat kernel(3,1,CV_8U,Scalar(1));//只在垂直方向膨胀，左右膨胀会把字符连在一块
+	dilate(zifu,zifu,kernel);
+	erode(zifu,zifu,kernel);
+	imshow("zifu",zifu);
+	//查找所有轮廓
+	vector<vector<Point>>contours;
+	findContours(zifu,contours,RETR_EXTERNAL,CHAIN_APPROX_SIMPLE);//查找所有外轮廓，输入图像必须为二值图
+	vector<vector<Point>>::const_iterator itc=contours.begin();//???????????????????????????????????????
+	vector<float> areas;
+	while (itc!=contours.end())
+	{
+		areas.push_back(itc->size());//??????????????????????????????????????????
+	}
+	sort(areas.begin(),areas.end());//???????????????????????????????????????????
+	/************************************************************************/
+	/*                                                                      */
+	/************************************************************************/
 
 #endif
 
